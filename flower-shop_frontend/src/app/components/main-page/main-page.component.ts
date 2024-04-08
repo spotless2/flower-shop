265f48532bf249862503a4045f0e3a7baa9c8ba9
Florin <florintudor.2k@gmail.com>
2024-04-09 00:54:10 +0300

    Updates for: - mainpage - navbar - footer - edit / add different styles

--- flower-shop_frontend/src/app/components/main-page/main-page.component.ts
+++ flower-shop_frontend/src/app/components/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
